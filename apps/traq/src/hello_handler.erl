8123e34766c2fcd9daa1a5a003d72934558bd7c6
Raphael Randschau <nicolai86@me.com>
2016-04-26 14:20:28 +0200

    Add sample http handler

---
+++ apps/traq/src/hello_handler.erl
@@ -0,0 +1,19 @@
+-module(hello_handler).
+-export([init/3, handle/2, terminate/2]).
+
+%% ===================================================================
+%% Cowboy callbacks
+%% ===================================================================
+
+init({_Any, http}, Req, []) ->
+    {ok, Req, []}.
+
+handle(Req, State) ->
+    {ok, Req2} = cowboy_req:reply(200,
+        [{<<"content-type">>, <<"text/plain">>}],
+        <<"Hello Erlang!">>,
+        Req),
+    {ok, Req2, State}.
+
+terminate(_Req, _State) ->
+    ok.
--- apps/traq/src/traq_app.erl
+++ apps/traq/src/traq_app.erl
@@ -15,6 +15,15 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Port = 8080,
+    NumOfAcceptors = 100,
+    io:format("Listening on ~p~n", [Port]),
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", hello_handler, []}]}
+    ]),
+    cowboy:start_http(my_http_listener, NumOfAcceptors, [{port, Port}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
     traq_sup:start_link().
 
 %%--------------------------------------------------------------------
